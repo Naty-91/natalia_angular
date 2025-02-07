1d8ea8a50067d8e94a34e35c0fa68607c68a0921
Naty-91 <natalia_fo@hotmail.com>
2025-02-07 13:21:18 +0100

    empezando el perfil de buscador y producto mas vendido

---
+++ src/app/features/supermarkets/supermarkets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supermarkets',
+  imports: [],
+  templateUrl: './supermarkets.component.html',
+  styleUrl: './supermarkets.component.scss'
+})
+export class SupermarketsComponent {
+
+}

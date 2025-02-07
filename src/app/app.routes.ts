7f3daf425b058fc1bb5fab4fcf7c9cfad4077617
Naty-91 <natalia_fo@hotmail.com>
2025-02-07 14:40:18 +0100

    historia de usuario terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { RegistrationComponent } from './features/registration/registration.component';
+import { SupermarketsComponent } from './features/supermarkets/supermarkets.component';
+import { BuscadorComponent } from './features/buscador/buscador.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'register', component: RegistrationComponent }
+  { path: 'register', component: RegistrationComponent },
+  {path:'supermarkets', component:SupermarketsComponent},
+  {path:'buscador', component:BuscadorComponent}
 ];
--- src/app/features/buscador/buscador.component.ts
+++ src/app/features/buscador/buscador.component.ts
@@ -1,11 +1,19 @@
+// buscador.component.ts
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-buscador',
-  imports: [],
   templateUrl: './buscador.component.html',
-  styleUrl: './buscador.component.scss'
+  styleUrls: ['./buscador.component.css']
 })
 export class BuscadorComponent {
+  searchText: string = '';
+  selectedCategory: string = '';
 
+  // Función que maneja la búsqueda
+  search() {
+    console.log('Buscando:', this.searchText, 'en categoría:', this.selectedCategory);
+    
+    // Aquí puedes hacer la búsqueda real, tal vez llamando a un servicio
+  }
 }
